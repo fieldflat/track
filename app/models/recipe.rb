199943ea903cb2fe7896e31b312c38051808059d
Tomonori HIRATA <hirata@sqlab.jp>
2020-04-04 22:13:31 +0900

    バリデーション追加

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :making_time, presence: true, length: { maximum: 100 }
+  validates :serves, presence: true, length: { maximum: 100 }
+  validates :ingredients, presence: true, length: { maximum: 300 }
+  validates :cost, presence: true
 end
