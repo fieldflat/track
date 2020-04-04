a07953ddacd9148255929da21da7605c38c54e4d
Tomonori HIRATA <hirata@sqlab.jp>
2020-04-04 21:53:59 +0900

    update: だいたい完成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,18 +2,18 @@ class RecipesController < ApplicationController
 
   def index
     @recipes = Recipe.all
-    render json: { recipes: @recipes }
+    render json: { recipes: @recipes.select(:id, :title, :making_time, :serves, :ingredients, :cost) }
   end
 
   def show
     @recipe = Recipe.find(params[:id])
-    render json: { message: @recipe, recipe: @recipe }
+    render json: { message: "Recipe details by id", recipe: {title: @recipe.title, making_time: @recipe.making_time, serves: @recipe.serves, ingredients: @recipe.ingredients, cost: @recipe.cost} }
   end
 
   def create
-    recipe = Recipe.new(recipe_params)
-    if recipe.save
-      render json: { message: 'Recipe successfully created!', recipe: recipe }
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save!
+      render json: { message: 'Recipe successfully created!', recipe: {title: @recipe.title, making_time: @recipe.making_time, serves: @recipe.serves, ingredients: @recipe.ingredients, cost: @recipe.cost} }
     else
       render json: { message: 'Recipe creation failed!', required: "title, making_time, serves, ingredients, cost" }
     end
@@ -21,11 +21,11 @@ class RecipesController < ApplicationController
 
   def update
     @recipe = Recipe.find(params[:id])
-    if @post.update(recipe_params)
-          render json: { status: 'SUCCESS', message: 'Updated the post', data: @post }
-        else
-          render json: { status: 'SUCCESS', message: 'Not updated', data: @post.errors }
-        end
+    if @recipe.update(recipe_params)
+      render json: { message: "Recipe successfully updated!", recipe: {title: @recipe.title, making_time: @recipe.making_time, serves: @recipe.serves, ingredients: @recipe.ingredients, cost: @recipe.cost} }
+    else
+      render json: { message: "Recipe updating failed" }
+    end
   end
 
   def destroy
@@ -40,7 +40,7 @@ class RecipesController < ApplicationController
 
   private
 
-  def post_params
+  def recipe_params
     params.require(:recipe).permit(:title, :making_time, :serves, :ingredients, :cost)
   end
 
